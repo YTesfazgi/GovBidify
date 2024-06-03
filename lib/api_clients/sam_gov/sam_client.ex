ff1cba733cb10382457ad24f0d3f8aab7de32794
Yohana Tesfazgi <yytesfazgi@gmail.com>
2024-06-03 14:49:45 -0400

    SamClient.search now returns {:ok, json}. Added documentation.

--- lib/api_clients/sam_gov/sam_client.ex
+++ lib/api_clients/sam_gov/sam_client.ex
@@ -6,8 +6,34 @@ defmodule SamClient do
   @sam_api_url Application.compile_env!(:gov_bidify, :sam_api_url)
   @sam_api_key Application.compile_env!(:gov_bidify, :sam_api_key)
 
+  @doc """
+  Performs a search via SAM.gov Get Opportunities API.
+
+  Returns `{:ok, json}`.
+
+  ## Example
+
+    iex> SamClient.search(%{"postedFrom" => "01/01/2018", "postedTo" => "05/10/2018", "ptype" => "a", "deptname" => "general"})
+    {:ok,
+      %{
+        "limit" => 10,
+        "links" => [
+          %{
+            "href" => "null/opportunities/v2/search?limit=10&postedFrom=01/01/2018&postedTo=05/10/2018&title=Driving",
+            "rel" => "self"
+          }
+        ],
+        "offset" => 0,
+        "opportunitiesData" => [],
+        "totalRecords" => 0
+      }
+    }
+  """
   def search(params) do
-    Finch.build(:get, build_url(params)) |> Finch.request(GovBidify.Finch)
+    {:ok, response} = Finch.build(:get, build_url(params))
+    |> Finch.request(GovBidify.Finch)
+
+    {:ok, _json} = Jason.decode(response.body)
   end
 
   defp build_url(params) do
