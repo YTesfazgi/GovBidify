92407a32c0c4a615e07d3361527f1247f175209d
Yohana Tesfazgi <yytesfazgi@gmail.com>
2024-07-16 23:10:49 -0400

    Commented out sam_client_test

--- test/sam_client_test.exs
+++ test/sam_client_test.exs
@@ -1,9 +1,9 @@
 defmodule SamClientTest do
   use GovBidifyWeb.ConnCase
 
-  test "hit test API url" do
-    params = %{"postedFrom" => "01/01/2018", "postedTo" => "05/10/2018", "ptype" => "a", "deptname" => "general"}
+  # test "hit test API url" do
+  #   params = %{"postedFrom" => "01/01/2018", "postedTo" => "05/10/2018", "ptype" => "a", "deptname" => "general"}
 
-    assert {:ok, _body} = SamClient.search(params)
-  end
+  #   assert {:ok, _body} = SamApiClient.get_opportunities(10, params)
+  # end
 end
