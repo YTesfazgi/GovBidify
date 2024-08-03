da159926cc8038cd32125b4fab788d756b9e3b72
Yohana Tesfazgi <yytesfazgi@gmail.com>
2024-08-03 15:25:02 -0400

    Updated opportunities search results to display tiles, drawer.

--- lib/gov_bidify_web/live/home_live.ex
+++ lib/gov_bidify_web/live/home_live.ex
@@ -4,14 +4,20 @@ defmodule GovBidifyWeb.HomeLive do
   alias GovBidify.Opportunities
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: nil, results: [])}
+    {:ok, assign(socket, query: nil, results: [], selected_opportunity: nil, drawer_visible: false)}
   end
 
   def handle_event("search", %{"query" => query}, socket) do
-    IO.inspect(query, label: "query")
     results = Opportunities.search_opportunities_by_title_and_description(query)
-    IO.inspect(results, label: "results")
-
     {:noreply, assign(socket, query: query, results: results)}
+  end
+
+  def handle_event("select_opportunity", %{"id" => notice_id}, socket) do
+    opportunity = Opportunities.get_opportunity_by_notice_id!(notice_id)
+    {:noreply, assign(socket, selected_opportunity: opportunity, drawer_visible: true)}
+  end
+
+  def handle_event("close_opportunity", _session, socket) do
+    {:noreply, assign(socket, selected_opportunity: nil, drawer_visible: false)}
   end
 end
