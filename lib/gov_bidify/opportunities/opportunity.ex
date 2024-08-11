87e08e651803cd986f3ea02a6dae659a6f65cdc2
Yohana Tesfazgi <yytesfazgi@gmail.com>
2024-08-11 13:52:05 -0400

    Added comments to schema for confusing/non-obvious fields.

--- lib/gov_bidify/opportunities/opportunity.ex
+++ lib/gov_bidify/opportunities/opportunity.ex
@@ -7,47 +7,47 @@ defmodule GovBidify.Opportunities.Opportunity do
     field :awardee, :string
     field :secondary_contact_phone, :string
     field :primary_contact_phone, :string
-    field :department_ind_agency, :string
+    field :department_ind_agency, :string # Department (L1)
     field :pop_zip, :string
     field :pop_state, :string
-    field :cgac, :string
-    field :office, :string
+    field :cgac, :string # Common Government-wide Accounting Classification
+    field :office, :string # Office (L3)
     field :pop_street_address, :string
     field :primary_contact_fax, :string
     field :pop_country, :string
-    field :naics_code, :string
-    field :sub_tier, :string
+    field :naics_code, :string # North American Industry Classification System
+    field :sub_tier, :string # Sub-tier (L2)
     field :pop_city, :string
     field :primary_contact_fullname, :string
-    field :award, :string
+    field :award, :string # Monetary amount
     field :archive_date, :date
     field :active, :string
     field :set_aside_code, :string
     field :zip_code, :string
-    field :organization_type, :string
+    field :organization_type, :string # Type of an organization -- department/sub-tier/office
     field :response_deadline, :utc_datetime
-    field :aac_code, :string
+    field :aac_code, :string # Activity Address Code
     field :primary_contact_title, :string
     field :secondary_contact_fax, :string
-    field :award_number, :string
-    field :link, :string
+    field :award_number, :string # Award ID
+    field :link, :string # Direct link to SAM notice
     field :additional_info_link, :string
     field :secondary_contact_email, :string
     field :fpds_code, :string
     field :description, :string
-    field :type, :string
+    field :type, :string # Current opportunity type
     field :primary_contact_email, :string
     field :secondary_contact_fullname, :string
     field :title, :string
     field :posted_date, :date
     field :award_date, :date
-    field :base_type, :string
-    field :sol, :string
+    field :base_type, :string # Original opportunity type
+    field :sol, :string # Solicitation ID
     field :city, :string
     field :classification_code, :string
     field :archive_type, :string
     field :secondary_contact_title, :string
-    field :set_aside, :string
+    field :set_aside, :string # Description of set aside
     field :state, :string
     field :country_code, :string
 
