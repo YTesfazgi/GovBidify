70ec6731160e39e60df3ea79341cdc9bb42b2e55
Yohana Tesfazgi <yytesfazgi@gmail.com>
2024-08-21 20:58:14 -0400

    Added datetime_handler.

--- lib/gov_bidify_web/live/home_live/home_live_html.ex
+++ lib/gov_bidify_web/live/home_live/home_live_html.ex
@@ -8,4 +8,27 @@ defmodule GovBidifyWeb.HomeLiveHTML do
     String.replace(string, "�", " ")
     # unless it is preceded by a comma (,)
   end
+
+  # def active_status(string) do
+  #   case string do
+  #     "YES" ->
+  #       "Active"
+
+  #     "NO" ->
+  #       "Inactive"
+
+  #     _ ->
+  #       ""
+  #   end
+  # end
+
+  def datetime_handler(datetime, date_string \\ "", time_string \\ ""), do: datetime_conversion(datetime, date_string, time_string)
+
+  defp datetime_conversion(nil, _date_string, _time_string), do: ""
+
+  defp datetime_conversion(datetime, date_string, time_string) do
+    date = DateTime.to_date(datetime)
+    time = DateTime.to_time(datetime)
+    "#{date_string}#{date.month}-#{date.day}-#{date.year}#{time_string}#{time}"
+  end
 end
