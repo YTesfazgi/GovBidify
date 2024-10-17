e50a77832b5905b2e41bb03f070196accb2da321
Yohana Tesfazgi <yytesfazgi@gmail.com>
2024-10-16 21:38:37 -0400

    Added test for home_live search functionality.

--- test/gov_bidify_web/live/home_live_test.exs
+++ test/gov_bidify_web/live/home_live_test.exs
@@ -1,9 +1,23 @@
 defmodule GovBidifyWeb.HomeLiveTest do
   use GovBidifyWeb.ConnCase
 
+  import GovBidify.Factory
   import Phoenix.LiveViewTest
 
   test "home page loads", %{conn: conn} do
     {:ok, _view, _html} = live(conn, ~p"/")
+  end
+
+  test "search form updates results based on URL params", %{conn: conn} do
+    insert(:opportunity, title: "Test Opportunity 1", description: "Some description", type: "Sources Sought")
+    insert(:opportunity, title: "Test Opportunity 2", description: "Another description", type: "Request for Proposals")
+
+    {:ok, view, _html} = live(conn, ~p"/?query=Test+Opportunity&flop%5Border_by%5D%5B%5D=response_deadline&flop%5Border_directions%5D%5B%5D=asc&flop%5Blimit%5D=10&flop%5Bfilters%5D=%7B%22active%22%3A%5B%22Yes%22%5D%2C%22type%22%3A%5B%22Sources+Sought%22%5D%7D")
+    assert render(view) =~ "Test Opportunity 1"
+    refute render(view) =~ "Test Opportunity 2"
+
+    {:ok, view, _html} = live(conn, ~p"/?query=Another&flop%5Border_by%5D%5B%5D=response_deadline&flop%5Border_directions%5D%5B%5D=asc&flop%5Blimit%5D=10&flop%5Bfilters%5D=%7B%22active%22%3A%5B%22Yes%22%5D%2C%22type%22%3A%5B%22Request+for+Proposals%22%5D%7D")
+    assert render(view) =~ "Test Opportunity 2"
+    refute render(view) =~ "Test Opportunity 1"
   end
 end
