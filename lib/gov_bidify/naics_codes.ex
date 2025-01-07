3829a72a966a040954189fdbfddd00c734a70ae1
Yohana Tesfazgi <yytesfazgi@gmail.com>
2025-01-07 13:53:18 -0500

    Cleans up formatting for list_naics_codes.

--- lib/gov_bidify/naics_codes.ex
+++ lib/gov_bidify/naics_codes.ex
@@ -5,7 +5,7 @@ defmodule GovBidify.NAICS_Codes do
   def list_naics_codes do
     Repo.all(NAICS_Code)
     |> Enum.reduce(%{}, fn naics, acc ->
-      Map.put(acc, naics.code, "#{naics.code} #{naics.description}")
+      Map.put(acc, naics.code, "#{naics.code} - #{naics.description}")
     end)
   end
 end
