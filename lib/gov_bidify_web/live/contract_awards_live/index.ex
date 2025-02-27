f5fcd973c239571ad12ee64bf65cfd0b26eca597
Yohana Tesfazgi <yytesfazgi@gmail.com>
2025-02-26 22:26:48 -0500

    Adds drawer to contract award search.

--- lib/gov_bidify_web/live/contract_awards_live/index.ex
+++ lib/gov_bidify_web/live/contract_awards_live/index.ex
@@ -1,23 +1,123 @@
 defmodule GovBidifyWeb.ContractAwardsLive.Index do
   use GovBidifyWeb, :live_view
-
+  alias GovBidifyWeb.Filter
+  alias GovBidifyWeb.SortMenu
   alias GovBidify.ContractAwards
+  alias GovBidify.NAICS_Codes
+  alias GovBidify.PSC_Codes
+  alias GovBidify.Opportunities
+  alias GovBidifyWeb.FlopDefaults
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: nil, results: [], selected_contract: nil, drawer_visible: false)}
+    form = to_form(FlopDefaults.contract_award_flop())
+
+    {:ok, assign(socket,
+      page_title: "Contract Awards",
+      form: form,
+      results: [],
+      meta: FlopDefaults.contract_award_meta(),
+      selected_contract_award: default_selected_contract_award(),
+      drawer_visible: false,
+      mobile_search_bar: true,
+      show_filter_modal: false,
+      combobox_options: combobox_options())}
   end
 
-  def handle_event("search", %{"query" => query}, socket) do
-    results = ContractAwards.search_contract_awards_by_description(query)
-    {:noreply, assign(socket, query: query, results: results)}
+  def handle_event("search", params, socket) do
+    # Merge new params with existing form data for mobile search
+    current_form_data = socket.assigns.form.source
+    merged_params = Map.merge(current_form_data, params)
+
+    form = merged_params
+    |> clean_params()
+    |> to_form()
+
+    updated_socket = assign(socket, form: form)
+
+    {:noreply, push_patch(updated_socket, to: ~p"/contract_awards?#{clean_params(merged_params)}")}
   end
 
-  def handle_event("select_contract", %{"id" => contract_transaction_unique_key}, socket) do
-    contract = ContractAwards.get_contract_award_by_contract_transaction_unique_key!(contract_transaction_unique_key)
-    {:noreply, assign(socket, selected_contract: contract, drawer_visible: true)}
+  def handle_event("select_contract_award", %{"id" => contract_transaction_unique_key}, socket) do
+    {:noreply,
+      socket
+      |> assign(selected_contract_award: ContractAwards.get_contract_award_by_contract_transaction_unique_key!(contract_transaction_unique_key))
+      |> push_event("open-drawer", %{})
+    }
   end
 
-  def handle_event("close_contract", _session, socket) do
-    {:noreply, assign(socket, selected_contract: nil, drawer_visible: false)}
+  def handle_event("close_contract_award", _session, socket) do
+    {:noreply, push_event(socket, "close-drawer", %{})}
+  end
+
+  def handle_params(%{"query" => query} = params, _uri, socket) do
+    IO.inspect(query, label: "query")
+    IO.inspect(params, label: "params")
+
+    form = params
+      |> clean_params()
+      |> to_form()
+
+    {results, meta} = ContractAwards.search(query, params)
+
+    {:noreply,
+      assign(socket,
+        results: results,
+        meta: meta,
+        form: form
+      )
+    }
+  end
+
+  def handle_params(_params, _uri, socket) do
+    {:noreply, socket}
+  end
+
+  defp clean_params(params) do
+    params
+    |> Map.delete("_target")
+    |> Map.update("filters", %{}, fn filters ->
+      filters
+      |> Enum.reject(fn {_key, value} ->
+        value == [""] || value == [] || is_nil(value) || value == ""
+      end)
+      |> Enum.into(%{})
+    end)
+    |> Enum.reject(fn {_key, value} ->
+      case value do
+        "" -> true
+        %{} -> Enum.empty?(value)
+        nil -> true
+        [""] -> true
+        [] -> true
+        _ -> false
+      end
+    end)
+    |> Enum.into(%{})
+  end
+
+  defp default_selected_contract_award do
+    %{
+      award_id_piid: nil,
+      solicitation_identifier: nil,
+      award_type: nil,
+      usaspending_permalink: nil,
+    }
+  end
+  defp combobox_options do
+    {_, options} = Cachex.fetch(:combobox_cache, :combobox_options, fn ->
+      %{
+        types: Opportunities.list_types(),
+        departments: Opportunities.list_departments(),
+        sub_tiers: Opportunities.list_sub_tiers(),
+        offices: Opportunities.list_offices(),
+        set_asides: Opportunities.list_set_asides(),
+        naics_codes: NAICS_Codes.list_naics_codes(),
+        psc_codes: PSC_Codes.list_psc_codes(),
+        countries: Opportunities.list_countries(),
+        states: Opportunities.list_states()
+      }
+    end)
+
+    options
   end
 end
